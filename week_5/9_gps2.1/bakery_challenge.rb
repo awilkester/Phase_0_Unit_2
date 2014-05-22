482be2af0f53a40c6f90b9699b9a1a0e1ed7d192
austinhay <hayaustin@gmail.com>
2014-05-22 08:32:51 -0700

    Finished commenting original code with pair.

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -1,50 +1,59 @@
 # U2.W5: The Bakery Challenge (GPS 2.1)
 
 # Your Names
-# 1)
-# 2)
+# 1) Austin Hay
+# 2) Jonathan Young
 
  # This is the file you should end up editing. 
- 
-def bakery_num(num_of_people, fav_food)
-  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
-  pie_qty = 0
-  cake_qty = 0
-  cookie_qty = 0
-  
-  has_fave = false
 
-  my_list.each_key do |k|
-  if k == fav_food
-  has_fave = true
-  fav_food = k
-  end
-  end
-  if has_fave == false
-  raise ArgumentError.new("You can't make that food")
-  else
-  fav_food_qty = my_list.values_at(fav_food)[0]
-  if num_of_people % fav_food_qty == 0
-    num_of_food = num_of_people / fav_food_qty
-    return "You need to make #{num_of_food} #{fav_food}(s)."
-  else num_of_people % fav_food_qty != 0
-    while num_of_people > 0
-    if num_of_people / my_list["pie"] > 0
-    pie_qty = num_of_people / my_list["pie"]
-    num_of_people = num_of_people % my_list["pie"]
-    elsif num_of_people / my_list["cake"] > 0
-    cake_qty = num_of_people / my_list["cake"]
-    num_of_people = num_of_people % my_list["cake"]
-    else
-    cookie_qty = num_of_people
-    num_of_people = 0
+def bakery_num(num_of_people, fav_food) # Defining a method called bakery_num with two parameters (number of people and favorite food)
+  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1} # My list is a hash with 3 keys and 3 values. Keys are the foods and the values are the #'s
+  pie_qty = 0 #Initial Quantities
+  cake_qty = 0 #Initial Quantities
+  cookie_qty = 0 #Initial Quantities
+  
+  has_fave = false # Seems unnecessary
+
+  my_list.each_key do |k| # Going to take each key in the my_list hash and assign it to an iterator variable k
+    if k == fav_food # If the key food is equal to user input for favorite food
+      has_fave = true # Condition is true
+      fav_food = k # Seems unnecessary, reiterating
     end
+  end
+  
+  if has_fave == false # If user's favorite food input isn't listed (i.e., isn't pie, cake, or cookie)
+    raise ArgumentError.new("You can't make that food") # User will receive an error message
+  else # If has_fave is true
+  fav_food_qty = my_list.values_at(fav_food)[0] # fav_food is going to be user input and can only be
+  # pie, cake, or cookie. values_at is going to choose the value to your fav_food input (remember it can
+  # only be pie, cake, or cookie). Thus we're going to be setting fav_food_qty to 8, 6, or 1.
+  
+  if num_of_people % fav_food_qty == 0 # if user input num_of_people divided by fav_food_qty has a remainder equal to 0
+    num_of_food = num_of_people / fav_food_qty # new variable num_of_food is equal to user input num_of_people 
+    # divided by fav_food_qty
+    return "You need to make #{num_of_food} #{fav_food}(s)." # Returns this to the screen
+  else num_of_people % fav_food_qty != 0 # SEEMS UNNECESSARY if line 31's condition is not met
+    while num_of_people > 0 # while user input num_of_people is greater than 0
+        if num_of_people / my_list["pie"] > 0 # If user input num_of_people divided by 8 is greater than 0
+          pie_qty = num_of_people / my_list["pie"] # new variable pie_qty is set equal to user input num_of_people
+          # divided by 8
+          num_of_people = num_of_people % my_list["pie"] #  num_of_people is now equal to the remainder of
+          # user input num_of_people divided by 8
+        elsif num_of_people / my_list["cake"] > 0 # If user input num_of_people divided by 6 is greater than 0
+          cake_qty = num_of_people / my_list["cake"] #new variable pie_qty is set equal to user input num_of_people
+          # divided by 6
+          num_of_people = num_of_people % my_list["cake"] #num_of_people is now equal to the remainder of
+          # user input num_of_people divided by 6
+        else
+          cookie_qty = num_of_people # new variable cookie_qty = user input num_of_people
+          num_of_people = 0
+        end
     end
+    
     return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
     end
   end
 end
- 
 
 #-----------------------------------------------------------------------------------------------------
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
