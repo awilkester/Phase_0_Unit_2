1e8646cb84d278c169a4df24fcb0edf529b91af3
austinhay <hayaustin@gmail.com>
2014-05-22 17:25:19 -0700

    Completed challenge with Stephen Estrada and added personal reflection.

--- week_5/5_virus_predictor/original_code.rb
+++ week_5/5_virus_predictor/original_code.rb
@@ -1,9 +1,20 @@
 # U2.W5: Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Stephen Estrada.
+
+# What is the program doing?
+
+# The program takes each state's population and region information and predicts the number of deaths that 
+
+# HASH -- State Data
+# The State Data file is a hash of hashes. IE The bigger hash has hashes inside of it. So in in the inner layer, the data elements are hashes with keys and values. Likewise the bigger state_data hash contains these inner hashes. This is similar to having multiple arrays in a single array.
+
+# Two different types of syntax:
+# 1) Strings used as keys
+# 2) Symbols used as keys
 
 # EXPLANATION OF require_relative
-#
+# Required relative is a rb file that contains all the data for this Virus Predictor. This data is stored in a giant hash that has keys associated with each state in the US. Each key has multiple values associated with population_density, population, the region and the regional spread. 
 #
 require_relative 'state_data'
 
@@ -17,43 +28,40 @@ class VirusPredictor
     @next_region = regional_spread
   end
 
+# This method below takes three parameters and uses them in its inner methods.
   def virus_effects  #HINT: What is the SCOPE of instance variables?
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
-  private  #what is this?  what happens if it were cut and pasted above the virus_effects method
+  private  
+  # Private is a method that hides everything below this line below from public viewing byt he user. 
+  # If private is moved about virus_effects then the user is unable to see the outcome of the deaths.
 
-  def predicted_deaths(population_density, population, state)
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else 
-      number_of_deaths = (@population * 0.05).floor
+# This method searches through the population density of each state and then discounts the number of people who will die and rounds down.
+  def predicted_deaths
+    case @population_density
+      when 0...50 then number_of_deaths = (@population * 0.05).floor
+      when 50...100 then number_of_deaths = (@population * 0.1).floor
+      when 100...150 then number_of_deaths = (@population * 0.2).floor
+      when 150...200 then number_of_deaths = (@population * 0.3).floor
+      else number_of_deaths = (@population * 0.04).floor
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# This method sorts by population density and increments the speed of the virus spread by increments of 0.5.
+  def speed_of_spread 
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else 
-      speed += 2.5
+    case @population_density
+      when 0...50 then speed += 2.5
+      when 50...100 then speed += 2.0
+      when 100...150 then speed += 1.5
+      when 150...200 then speed += 1.0
+      else speed += 0.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -62,11 +70,16 @@ class VirusPredictor
 
 end
 
+STATE_DATA.each{|k,v| VirusPredictor.new(k, v[:population_density], v[:population], v[:region], v[:regional_spread]).virus_effects}
+
+# OR (both work the same but with different method approaches)
+
+STATE_DATA.map {|k,v| VirusPredictor.new(k, v[:population_density], v[:population], v[:region], v[:regional_spread]).virus_effects}
+
 #=======================================================================
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
-
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population], STATE_DATA["Alabama"][:region], STATE_DATA["Alabama"][:regional_spread]) 
 alabama.virus_effects
@@ -79,3 +92,17 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population], STATE_DATA["Alaska"][:region], STATE_DATA["Alaska"][:regional_spread]) 
 alaska.virus_effects
+
+# REFACTORING:
+#     1.) We took out the inputs for virus_effects because they were redundant -- instance variables can be called from anywhere across the function.
+
+#     2.) We simplified both the virus methods in order to remove the inputs and use case statements to simplify the logic.
+
+# Reflection:
+
+=begin 
+This challenge was one of the most difficult for the week in my opinion. I found it quite difficult to find the correct methods needed to solve the challenge and also to implement them in such a way as to fulfill all the requirements. I also found the driver code a bit confusing and it wasn't clear upfront what the conditions were. I spent 90% of the time debugging when in reality the code for this challenge was not all that difficult to understand the code up. 
+
+Luckily I had an awesome pairing partner and we worked through everything!
+=end
+
